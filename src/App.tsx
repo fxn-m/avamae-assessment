5dec1ebd7cfd667a0e6891188ad5657fe7d139ce
felix <67316355+fxn-m@users.noreply.github.com>
2024-11-16 01:05:23 +0000

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,21 @@ import NavBar from "./components/NavBar";
 function App() {
     return (
         <Router>
-            <div className="App">
+            <div className="App flex h-screen flex-col">
                 <NavBar />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about-us" element={<AboutUs />} />
                     <Route path="/contact-us" element={<ContactUs />} />
                 </Routes>
+                <div className="footer mx-auto mt-auto flex h-16 w-4/5 flex-row-reverse items-center border-t text-xs font-light text-gray-400">
+                    <p>
+                        Website Development by{" "}
+                        <a className="underline" href="https://www.avamae.co.uk/">
+                            AVAMAE
+                        </a>
+                    </p>
+                </div>
             </div>
         </Router>
     );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,19 +2,29 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
     return (
-        <nav className="h-14 border border-b-gray-200">
-            <div className="container mx-auto flex items-center justify-between">
-                <div className="flex items-center">
-                    <img src="./logo.png" className="h-12" />
+        <nav className="h-16 border-b border-gray-200">
+            <div className="container mx-auto flex h-full items-center justify-between">
+                <div className="flex items-center space-x-3">
+                    <img src="./logo.png" className="h-10" alt="Logo" />
                     <p className="text-xl font-bold">COMPANY</p>
                 </div>
+
                 <div className="flex items-center space-x-10">
-                    <div className="items-center space-x-10 text-xs tracking-widest text-gray-600">
-                        <Link to="/">HOME</Link>
-                        <Link to="/about-us">ABOUT US</Link>
-                        <Link to="/contact-us">CONTACT US</Link>
+                    <div className="flex space-x-10 text-xs tracking-wider text-gray-600">
+                        <Link to="/" className="hover:text-blue-500">
+                            HOME
+                        </Link>
+                        <Link to="/about-us" className="hover:text-blue-500">
+                            ABOUT US
+                        </Link>
+                        <Link to="/contact-us" className="hover:text-blue-500">
+                            CONTACT US
+                        </Link>
                     </div>
-                    <div className="inline-flex rounded-md bg-blue-100 px-8 py-3 text-xs font-bold tracking-normal text-blue-400">Log in</div>
+
+                    <Link to="/login" className="rounded-md bg-blue-100 px-7 py-3 text-xs font-semibold text-blue-600 hover:bg-blue-200">
+                        Log in
+                    </Link>
                 </div>
             </div>
         </nav>
